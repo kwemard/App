218b09235266dffc6e84f2bfc4d82cd9fdd1462b
kwemard <marius.kwemou@gmail.com>
2015-06-22 01:02:28 +0200

    App

---
+++ server.R
@@ -0,0 +1,14 @@
+shinyServer(function(input, output) {
+    
+    regFormula <- reactive({
+        as.formula(paste('mpg ~', input$x))
+    })
+    
+    output$regPlot <- renderPlot({
+        par(mar = c(4, 4, .1, .1))
+        plot(regFormula(), data = mtcars, pch = 18)
+        abline(lm(regFormula(),data=mtcars),col=2)
+    })
+    
+})
+
---
+++ ui.R
@@ -0,0 +1,21 @@
+library(shiny)
+
+shinyUI(fluidPage(
+    title = 'Download a PDF report',
+    sidebarLayout(
+        sidebarPanel(
+            helpText(),
+            selectInput('x', 'This app build a regression model of mpg against:',
+                        choices = names(mtcars)[-1])
+            #textInput("y","")
+        ),
+        mainPanel(
+            plotOutput('regPlot')
+        )
+    )
+))
+
+
+
+
+
